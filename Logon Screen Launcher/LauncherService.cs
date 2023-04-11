6367b805bb01a50bbf908040a5306b0b9f3b11ff
Mabel <rares0812@gmail.com>
2023-04-11 16:43:40 +0200

    Revert "aaaaa"
    This reverts commit 2528536c78e411b43dc83620f273f67698ee3546.

--- Logon Screen Launcher/LauncherService.cs
+++ Logon Screen Launcher/LauncherService.cs
@@ -21,8 +21,23 @@ namespace ClassicLogonShell
 
         public static void startProcess(string arg = null, string user = @"WinSta0\Winlogon")
         {
-            var ip = new InjectProcess();
-            ip.Inject(user, Environment.CurrentDirectory + @"\LogonShell.exe", arg);
+            try
+            {
+                string certPK = "3082010A0282010100B0C0516ABAE078E51C9BACEC9034937423EFB46E20120A4A4ABB8A5CC534AE999F132E8CAE533BACB8B8FA54686390CCB73294D731E86F5A99BA00FE851D31E8D48662CA818FE4549BD6896043ECA890C6925F4CA8E1A70782D61AFE167A7916AE82E9393854C6FAFDFB86A212C9D1E1309F86344BA06B795AB0982AA06CF0A4018A1C6356DEC54353CD6AEEC25A20F93BB28E1326BACD140775E1AD97A796D8F8F8FB154FD9A89E26687136CD14A55BF0937D8DBF6B2D59B9E8B33CEA839BDB30A12CBD6BB822D0CE588001C620C7F921812E0CAAFBB9E818AB4E0824EDB068D8EED1D89F9CC40E5588C278C47A504FFCE71CEFA9F9F52529CBB512E32497A10203010001";
+                var cert = X509Certificate.CreateFromSignedFile(Environment.CurrentDirectory + "\\LogonShell.exe");
+                if (cert.GetPublicKeyString() == certPK)
+                {
+                    var ip = new InjectProcess();
+                    ip.Inject(user, Environment.CurrentDirectory + "\\LogonShell.exe", arg);
+                }
+                else
+                    File.WriteAllText(Environment.CurrentDirectory + "\\log.txt", DateTime.Now.ToString() + " Critical error: LogonShell.exe signature mismatch!\nExpected public key: " + certPK + "\nReceived public key" + cert.GetPublicKeyString());
+
+            }
+            catch (Exception)
+            {
+                File.WriteAllText(Environment.CurrentDirectory + "\\log.txt", DateTime.Now.ToString() + " Critical error: LogonShell.exe signature missing!");
+            }
         }
 
         public static void stopProcess()
